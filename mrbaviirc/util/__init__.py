de4849da4e151687af807cc511e57b03c8de1684
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2017-10-08 11:12:26 -0400

    Removed "updateall" utility decorator

--- mrbaviirc/util/__init__.py
+++ mrbaviirc/util/__init__.py
@@ -4,7 +4,9 @@ __author__      =   "Brian Allen Vanderburg II"
 __copyright__   =   "Copyright (C) 2017 Brian Allen Vanderburg II"
 __license__     =   "Apache License 2.0"
 
+__all__ = []
 
+__all__.append("updatedoc")
 def updatedoc(prepend, append):
     """ A decorator to prepend or append to an object's doc string. """
     def wrapper(obj):
@@ -17,36 +19,4 @@ def updatedoc(prepend, append):
 
     return wrapper
 
-
-def updateall(item_or_name):
-    """
-        Update the __all__ list of the calling module with the function or class name. 
-        
-            @updateall
-                def fn(...):
-                ...
-
-            @updateall
-                class cls(...):
-                ...
-
-        This method also supports updating the __all_ list with variables:
-
-        updateall("var")
-        var = var1
-    """
-    from types import StringTypes
-    import inspect
-
-    caller = inspect.stack()[1]
-    module = inspect.getmodule(caller[0])
-    real_all = module.__all__
-
-    if isinstance(item, StringTypes):
-        # Being calld directly, not as a decorator
-        real_all.append(item)
-    else:
-        # Being called as a decorator
-        real_all.append(item.__name__)
-        return item
 
