725e3a8c469691e6d8832d4d7442f2dabfc5830d
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2020-01-05 13:58:55 -0500

    More changes to hooks.
    Separated Hooks and Signal so they are not derived from the other.
    
    Provided a process method which will yield the result of each called
    callback.  This can be used in place of get to avoid having a reference
    to a callback when the parameters are the same for each call:
    
        Instead of:
    
            sum = 0
            for cb in h.get("name"):
                sum += cb(10)
    
        You can do:
    
            sum = 0
            for value in h.process("name", 10):
                sume += value
    
    The get method is still available when it is needed, such as calling
    callbacks with different values.  It is also possible to terminal the
    callbacks early from get can process simply by not completing the
    iterations.

--- mrbaviirc/common/hooks.py
+++ mrbaviirc/common/hooks.py
@@ -4,19 +4,19 @@ __author__ = "Brian Allen Vanderburg II"
 __copyright__ = "Copyright (C) 2018-2019 Brian Allen Vanderburg II"
 __license__ = "Apache License 2.0"
 
-__all__ = ["Hooks", "HookEntry"]
+__all__ = ["CallbackEntry", "Hooks", "Signal"]
 
 
 import threading
-from typing import Callable, Any
+from typing import Optional, Callable, Any, Generator
 import weakref
 
 
-class HookEntry:
-    """ Represent a registered hook entry. """
-    def __init__(self, hooks, hook, callback):
-        self._hooks = weakref.ref(hooks)
-        self._hook = hook
+class CallbackEntry:
+    """ Represent a registered callback entry. """
+    def __init__(self, container, callback, data=None):
+        self._container = weakref.ref(container)
+        self._data = data
 
         try:
             obj = callback.__self__
@@ -86,9 +86,9 @@ class HookEntry:
 
     def unregister(self):
         """ Unregister this hook. """
-        hooks = self._hooks()
-        if hooks is not None:
-            hooks.unregister(self)
+        container = self._container()
+        if container is not None:
+            container.unregister(self)
 
     def _cleanup(self, ref): # pylint: disable=unused-argument
         """ Unregister the hook when ref is zero. """
@@ -111,7 +111,7 @@ class Hooks:
         self._hooks = {}
         self._queue = []
 
-    def register(self, hook: str, callback: Callable[..., Any]) -> HookEntry:
+    def register(self, hook: str, callback: Callable[..., Any]) -> CallbackEntry:
         """ Add a callback for a given hook.
 
         Parameters
@@ -127,27 +127,27 @@ class Hooks:
 
         Returns
         -------
-        HookEntry
+        CallbackEntry
             An object that identifies this entry in the hooks.
         """
 
-        entry = HookEntry(self, hook, callback)
+        entry = CallbackEntry(self, callback, hook)
         with self._lock:
             queue = self._hooks.setdefault(hook, [])
             queue.append(entry)
 
         return entry
 
-    def unregister(self, entry: HookEntry):
+    def unregister(self, entry: CallbackEntry):
         """ Remove a hook a previously registered hook callback.
 
         Parameters
         ----------
-        entry : HookEntry
+        entry : CallbackEntry
             The entry of a previously registered hook.
 
         """
-        hook = entry._hook # pylint: disable=protected-access
+        hook = entry._data # pylint: disable=protected-access
         with self._lock:
             queue = self._hooks.get(hook, None)
             if queue is not None:
@@ -156,11 +156,27 @@ class Hooks:
                 except ValueError:
                     pass
 
-    def call(self, hook: str, *args, **kwargs):
-        """ Call all registered callbacks for a given hook.
+    def process(self, hook: str, *args, **kwargs) -> Generator[Any, None, None]:
+        """ Process all registered callbacks for a hook, yielding the results.
 
-        Note that it is possible for two different threads to call the same
-        hook at the same time.
+        Note that it is possible for two different threads to process the same
+        hook at the same time.  This method is intended to replace the original
+        accumulate method.  Instead of providing a callback as follows:
+
+            def sum_callback(value):
+                global sum_value
+                sum_value += value
+
+            hook.accumulate("myhook", sum_callback, arg1, arg2)
+
+        The code can be more cleanly written as:
+
+            sum_value = 0
+            for cur_value in hook.process("myhook", arg1, arg2):
+                sum_value += cur_value
+
+        This also allows to abort processing the hooks by simply not iterating
+        over any more values.
 
         Parameters
         ----------
@@ -170,6 +186,10 @@ class Hooks:
             Positional parameters to pass to the callbacks
         **kwargs
             Keyword parameters to pass to the callbacks
+
+        Yields
+        ------
+        The results of the callbacks called.
         """
 
         # Release the lock before iterating so multiple threads can call hooks
@@ -181,17 +201,35 @@ class Hooks:
         for entry in queue:
             callback = entry.callback
             if callback is not None:
-                callback(*args, **kwargs)
+                result = callback(*args, **kwargs)
+                callback = None # release the reference as soon as possible
+                yield result
+
+    def call(self, hook: str, *args, **kwargs):
+        """ Call all registered callbacks for a given hook.
+
+        Parameters
+        ----------
+        See the `process` method
+        """
+
+        # We just process all callbacks and discard the results
+        # Is there a more pythonic way to do this?
+        for result in self.process(hook, *args, **kwargs): # pylint: disable=unused-variable
+            result = None
 
     def get(self, hook: str):
         """ Return a generator which will yield the callbacks.
 
-        This method replaces the previous accumulate method. Note that the
-        generator returns hard references to the callback. This usually isn't
-        a problem if the value is only stored to a local variable in a function
-        while iterating over it as the variable reference will be released
-        by the end of the function. Otherwise, the variable stored to should be
-        set to None after the iteration is complete
+        Note that the generator returns hard references to the callback. This
+        usually isn't a problem if the value is only stored to a local variable
+        in a function while iterating over it as the variable reference will be
+        released by the end of the function. Otherwise, the variable stored to
+        should be set to None after the iteration is complete.  This method
+        can be used to call each callback with different parameters.  If all
+        callbacks of a hook will be called with the same parameters, just use
+        the `call` method if results are not needed or the `process` method
+        if the results are needed.
 
         To understand the issue:
 
@@ -221,7 +259,7 @@ class Hooks:
             Each registered hook callback is returned as long as it is still
             enabled and live if it is a weak reference.
         """
-        # clone and release just like in call
+        # clone and release lock just like in call
         with self._lock:
             queue = tuple(self._hooks.get(hook, []))
 
@@ -229,6 +267,55 @@ class Hooks:
             callback = entry.callback
             if callback is not None:
                 yield callback
+
+
+    def first(self, hook: str) -> Optional[Callable[..., Any]]:
+        """ Return the first calback for the given hook.
+
+        Note, this returns a hard reference so the result should be set to
+        None as soon as convenient.
+
+        Parameters
+        ----------
+        hook : str
+            The name of the hook.
+
+        Returns
+        -------
+        Callable[..., Any]
+            The first active regisistered callback for the hook.
+        None
+            If no registered callback for the hook is active.
+        """
+
+        for callback in self.get(hook):
+            return callback # Return the first item
+
+        return None
+
+    def last(self, hook: str) -> Optional[Callable[..., Any]]:
+        """ Return the last callback for the given hook.
+
+        Note, this returns a hard reference so the result should be set to
+        None as soon as convenient.
+
+        Parameters
+        ----------
+        hook : str
+            The name of the hook.
+
+        Returns
+        -------
+        Callable[..., Any]
+            The last active regisistered callback for the hook.
+        None
+            If no registered callback for the hook is active.
+        """
+        result = None
+        for callback in self.get(hook):
+            result = callback
+
+        return result
 
     def queue(self, hook, *args, **kwargs):
         """ Queue a hook call for later calling.
@@ -254,32 +341,91 @@ class Hooks:
                 self.call(hook, *args, **kwargs)
 
 
-class Signal(Hooks):
+class Signal:
     """ This class represents a single hook to be used as a signal object.
 
-    This class provides all the same functions as the Hooks class, but does
-    not support named hook registrations.  In this way, it is useful as a
-    simple signal mechanism.  See the parent class for more detailed
-    documentation.  The only difference is the varoius methods do not take
-    a named hook, but instead just a single name internally
+    This class provides most all the same functions as the Hooks class, but
+    does not use named hook registrations. In this way, it is useful as a
+    simple signal mechanism.  See the Hooks class for the method details.
+    The only difference is the methods do not take a "hook" parameter.
     """
 
-    def register(self, callback: Callable[..., Any]) -> HookEntry:
-        # pylint: disable=arguments-differ
-        """ See parent class """
-        return Hooks.register(self, "hook", callback)
+    def __init__(self):
+        """ Create the signal data. """
+        self._lock = threading.RLock()
+        self._entries = []
+        self._queue = []
+
+    def register(self, callback: Callable[..., Any]) -> CallbackEntry:
+        """ See `Hooks.register` """
+
+        entry = CallbackEntry(self, callback)
+        with self._lock:
+            self._entries.append(entry)
+
+        return entry
+
+    def unregister(self, entry: CallbackEntry):
+        """ See `Hooks.unregister` """
+
+        with self._lock:
+            try:
+                self._entries.remove(entry)
+            except ValueError:
+                pass
+
+    def process(self, *args, **kwargs):
+        """ See `Hooks.process` """
+
+        # clone and release lock to allow multiple threads to call at the same time
+        with self._lock:
+            entries = tuple(self._entries)
+
+        for entry in entries:
+            callback = entry.callback
+            if callback is not None:
+                result = callback(*args, **kwargs)
+                callback = None # release reference asap
+                yield result
 
     def call(self, *args, **kwargs):
-        # pylint: disable=arguments-differ
-        """ See parent class """
-        return Hooks.call(self, "hook", *args, **kwargs)
+        """ See `Hooks.call` """
+        for result in self.process(*args, **kwargs): # pylint: disable=unused-variable
+            result = None
 
     def get(self):
-        # pylint: disable=arguments-differ
-        """ See parent class """
-        return Hooks.get(self, "hook")
+        """ See `Hooks.get` """
+        with self._lock:
+            entries = tuple(self._entries)
+
+        for entry in entries:
+            callback = entry.callback
+            if callback is not None:
+                yield callback
+
+    def first(self):
+        """ See `Hooks.first` """
+        for callback in self.get():
+            return callback # first item
+
+        return None
+
+    def last(self):
+        """ See `Hooks.last` """
+        result = None
+        for callback in self.get():
+            result = callback
+
+        return result
 
     def queue(self, *args, **kwargs):
-        # pylint: disable=arguments-differ
-        """ See parent class. """
-        return Hooks.queue("hook", *args, **kwargs)
+        """ See `Hooks.queue` """
+        with self._lock:
+            self._queue.append((args, kwargs))
+
+    def flush(self):
+        """ See `Hooks.flush` """
+        with self._lock:
+            while self._queue:
+                (args, kwargs) = self._queue.pop(0)
+                self.call(*args, **kwargs)
